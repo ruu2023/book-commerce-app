e30112355457d89ca5bbfefd9d93adc85f163c3a
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2024-09-18 23:47:36 +0900

    bcrypt and credential setting

--- app/lib/next-auth/options.ts
+++ app/lib/next-auth/options.ts
@@ -1,6 +1,8 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 import prisma from "../prisma";
+import bcrypt from "bcrypt";
 import { NextAuthOptions } from "next-auth";
 export const nextAuthOptions: NextAuthOptions = {
   debug: false,
@@ -9,9 +11,47 @@ export const nextAuthOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        if (user && credentials?.password && user.password) {
+          // パスワードの検証
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (isValid) {
+            return user;
+          }
+        }
+
+        // 認証失敗
+        return null;
+      },
+    }),
   ],
+  pages: {
+    signIn: "/auth/signin", // カスタムサインインページ
+  },
+  session: {
+    strategy: "jwt",
+  },
   adapter: PrismaAdapter(prisma),
   callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
     session: ({ session, user }) => {
       return {
         ...session,
