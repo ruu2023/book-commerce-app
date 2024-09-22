a6b58bcb5dc5965e6685c28fc8e1999ce37f33a5
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2024-09-22 09:46:33 +0900

    sign in with guest

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default async function Home() {
     <>
       <main className="flex flex-wrap justify-center items-center md:mt-32 mt-20">
         <h2 className="text-center w-full font-bold text-3xl mb-2">
-          Book Commerce
+          Buy the book
         </h2>
         {contents.map((book: BookType) => (
           <Book
